2b65cf016dddd4a28e420db426c75b0d4ee64e94
gustavo.caruso <gustavoalmeida26@hotmail.com>
2021-09-30 14:40:02 -0300

    add tird requirement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
@@ -19,6 +20,16 @@ class App extends React.Component {
           isSaveButtonDisabled={ false }
           onInputChange={ callback }
           onSaveButtonClick={ callback2 }
+        />
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
         />
       </div>
     );
