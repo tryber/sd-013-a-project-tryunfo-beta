a00eb015dd0e8b0c0f4a9b4393ff6578d59176a1
Gabriel Gaspar <gabrielgaspar447@gmail.com>
2021-09-29 22:25:49 -0300

    Req 05

--- src/App.js
+++ src/App.js
@@ -15,14 +15,45 @@ class App extends React.Component {
       cardAttr3: '',
       cardRare: '',
       cardTrunfo: false,
+      isSaveButtonDisabled: true,
     };
+  }
+
+  checkSaveBtnStatus = () => {
+    const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3, cardImage,
+      cardRare } = this.state;
+
+    const attr1 = parseInt(cardAttr1, 10);
+    const attr2 = parseInt(cardAttr2, 10);
+    const attr3 = parseInt(cardAttr3, 10);
+    const maxAttrSum = 210;
+    const attrTop = 90;
+
+    const noEmptyTextInputs = (
+      cardName.length > 0 && cardDescription.length > 0
+      && cardImage.length > 0 && cardRare.length > 0
+    );
+
+    const attrSumInRange = (attr1 + attr2 + attr3) <= maxAttrSum;
+
+    const attrsInRange = (
+      attr1 >= 0 && attr1 <= attrTop
+      && attr2 >= 0 && attr2 <= attrTop
+      && attr3 >= 0 && attr3 <= attrTop
+    );
+
+    if (noEmptyTextInputs && attrSumInRange && attrsInRange) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
   }
 
   onInputChange = ({ target: { id, value, checked } }) => {
     if (id === 'cardTrunfo') {
-      this.setState({ [id]: checked });
+      this.setState({ [id]: checked }, () => this.checkSaveBtnStatus());
     } else {
-      this.setState({ [id]: value });
+      this.setState({ [id]: value }, () => this.checkSaveBtnStatus());
     }
   }
 
