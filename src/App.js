1ed679586c3d5a05293b9269aa6723f758585d9c
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-10-02 14:13:11 -0300

    add os components no App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import Card from './components/Card';
+import FilterCards from './components/FilterCards';
+import Form from './components/Form';
 
 class App extends Component {
   constructor(props) {
@@ -23,7 +26,16 @@ class App extends Component {
 
   render() {
     return (
-      <h1>Test App</h1>
+      <div className="container-tryunfo">
+        <div className="container-preview">
+          <div><Form /></div>
+          <div><Card /></div>
+        </div>
+        <div className="container-display-card">
+          <div><FilterCards /></div>
+          <div><Card /></div>
+        </div>
+      </div>
     );
   }
 }
