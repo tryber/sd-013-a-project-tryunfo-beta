6066e91238bda59bc48dbc2d91eb4756393052ed
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-10-02 14:05:19 -0300

    foi add todas as props de app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
-import Componente from './components';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cardList: [],
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      filterName: '',
+      filterRaridade: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+  }
+
   render() {
     return (
-      <div>
-        <h1>Tryunfo</h1>
-        <Componente />
-      </div>
+      <h1>Test App</h1>
     );
   }
 }
