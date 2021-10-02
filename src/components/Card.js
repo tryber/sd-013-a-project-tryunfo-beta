e6c6754976261a5a64168761303da954abb42d14
Rogerio P. Da Silva <rogeriop1990cv@gmail.com>
2021-10-02 14:09:04 -0300

    Foi criando os render padrao dos componentes

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class Card extends Component {
+  render() {
+    return (
+      <h1>Cards</h1>
+    );
+  }
+}
+
+export default Card;
---
+++ src/components/FilterCards.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class FilterCards extends Component {
+  render() {
+    return (
+      <h1>Filter Cards</h1>
+    );
+  }
+}
+
+export default FilterCards;
